f614a8790ca417ff2e7448cd9a97ac45b9285af8
hjalmar <hjalmar.thunberg@gmail.com>
2020-09-07 14:53:31 +0200

    Fixed mongodb link

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 var history = require('connect-history-api-fallback');
 
 // Variables
-var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/animalDevelopmentDB'; // TODO: change this
+var mongoURI = process.env.MONGODB_URI || 'mongodb+srv://g20admin:12345@cluster0.mw6yd.mongodb.net/pubcrawl?retryWrites=true&w=majority';
 var port = process.env.PORT || 3000;
 
 // Connect to MongoDB
