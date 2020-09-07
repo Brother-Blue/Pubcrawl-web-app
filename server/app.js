aa99dda7ed91167a2edfa2387b48d034f32e25b6
hjalmar <hjalmar.thunberg@gmail.com>
2020-09-07 14:46:49 +0200

    test commit fron editor

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 var history = require('connect-history-api-fallback');
 
 // Variables
-var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/animalDevelopmentDB';
+var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/animalDevelopmentDB'; // TODO: change this
 var port = process.env.PORT || 3000;
 
 // Connect to MongoDB
