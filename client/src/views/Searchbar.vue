3cd305339cc24fde76d4d67e32763644840fdb8f
Hugo Hempel <Hugohhempel@gmail.com>
2020-09-23 13:58:40 +0200

    added searchbar component

---
+++ client/src/views/Searchbar.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <!-- Search bar -->
+
+<form class="searchbar">
+  <div>
+    <!-- Input -->
+    <input class="searchbar-input" type="search" placeholder="Enter bar name">
+    <!-- Clear button -->
+    <a href="#" class="searchbar-clear"></a>
+  </div>
+</form>
+</template>
+
+<script>
+export default {
+  name: 'searchbar'
+}
+</script>
