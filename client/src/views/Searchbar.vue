8d9a6c0154815263b9d1d8aa6f7ffc296a915274
Hugo Hempel <Hugohhempel@gmail.com>
2020-09-23 14:02:52 +0200

    removed  comments

--- client/src/views/Searchbar.vue
+++ client/src/views/Searchbar.vue
@@ -1,12 +1,8 @@
 <template>
 
-  <!-- Search bar -->
-
 <form class="searchbar">
   <div>
-    <!-- Input -->
     <input class="searchbar-input" type="search" placeholder="Enter bar name">
-    <!-- Clear button -->
     <a href="#" class="searchbar-clear"></a>
   </div>
 </form>
