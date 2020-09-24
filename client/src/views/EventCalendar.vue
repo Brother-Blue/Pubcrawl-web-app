dc7678aa92195a590a0e819cad6d731fc5471ef4
Christian_O'Neill <gusonech@student.gu.se>
2020-09-24 10:10:50 +0200

    Added pathing to events calendar page

---
+++ client/src/views/EventCalendar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <pubcrawl-jumbotron></pubcrawl-jumbotron>
+    <p>Hi :)</p>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+  name: 'event-calendar',
+  components: {
+    'pubcrawl-jumbotron': Header
+  },
+  data() {
+    return {
+      // Soemthing
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
