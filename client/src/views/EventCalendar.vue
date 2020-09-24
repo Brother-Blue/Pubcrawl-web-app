72b15aab32daf006b2c4826ef4aa9e9e37d7e8e3
Christian_O'Neill <gusonech@student.gu.se>
2020-09-24 15:02:01 +0200

    Added functionality to calendar.
    Calendar now gets events for specific days

--- client/src/views/EventCalendar.vue
+++ client/src/views/EventCalendar.vue
@@ -7,22 +7,19 @@
     hide-header="True"
     start-weekday="1"
     v-model="value"
-    :min="minDate"
-    :max="maxDate"
+    :min="min"
+    :max="max"
+    @selected="getEvents(value)"
     ></b-calendar>
-    <!-- <b-button v-on:click="getEvents()">Get events!</b-button>
     <div v-for="event in events" :key="event">
         <p>{{event}}</p>
-    </div> -->
-    <div class="event-list">
-        <div></div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
-// import { Api } from '@/Api'
+import { Api } from '@/Api'
 
 export default {
   name: 'event-calendar',
@@ -46,20 +43,23 @@ export default {
     }
   },
   methods: {
-    getEvents() {
-    //   Api.get('/events')
-    //     .then(response => {
-    //       // this.events = response.data.events[0].title
-    //       var e = response.data.events
-    //       var eArr = []
-    //       for (var i = 0; i < e.length; i++) {
-    //         eArr.push(e[i].title, e[i].startDate, e[i].endDate, e[i].createdAt)
-    //       }
-    //       this.events = eArr
-    //     })
-    //     .catch(error => {
-    //       this.events = error
-    //     })
+    getEvents(day) {
+      Api.get('/events')
+        .then(response => {
+          var e = response.data.events
+          var eArr = []
+          for (var i = 0; i < e.length; i++) {
+            if (e[i].startDate.substring(0, 10) === day) {
+              eArr.push(e[i].title, e[i].startDate, e[i].endDate, e[i].createdAt)
+            } else {
+              continue
+            }
+          }
+          this.events = eArr
+        })
+        .catch(error => {
+          this.events = error
+        })
     }
   }
 }
