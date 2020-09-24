834bbd58b0c9fb0da59628f15031245a0ca099e3
Christian_O'Neill <gusonech@student.gu.se>
2020-09-24 13:36:25 +0200

    Added calendar to event calendar page

---
+++ client/src/components/EventItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'event-item',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- client/src/views/EventCalendar.vue
+++ client/src/views/EventCalendar.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
     <pubcrawl-jumbotron></pubcrawl-jumbotron>
-    <b-button v-on:click="getEvents()">Get events!</b-button>
+    <b-calendar
+    block
+    locale="en"
+    hide-header="True"
+    start-weekday="1"
+    v-model="value"
+    :min-date="minDate"
+    :max-date="maxDate"
+    ></b-calendar>
+    <!-- <b-button v-on:click="getEvents()">Get events!</b-button>
     <div v-for="event in events" :key="event">
         <p>{{event}}</p>
+    </div> -->
+    <div class="event-list">
+        <div></div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
-import { Api } from '@/Api'
+// import { Api } from '@/Api'
 
 export default {
   name: 'event-calendar',
@@ -18,25 +30,36 @@ export default {
     'pubcrawl-jumbotron': Header
   },
   data() {
+    const date = new Date()
+    const today = new Date(date.getFullYear(), date.getMonth(), date.getDate())
+    const minDate = new Date(today)
+    const maxDate = new Date(today)
+    minDate.setMonth(today.getMonth())
+    minDate.setDate(today.getDate())
+    maxDate.setMonth(today.getMonth() + 2)
+    maxDate.setDate(today.getDate())
     return {
-      events: null
+      events: null,
+      value: '',
+      minDate: minDate,
+      maxDate: maxDate
     }
   },
   methods: {
     getEvents() {
-      Api.get('/events')
-        .then(response => {
-          // this.events = response.data.events[0].title
-          var e = response.data.events
-          var eArr = []
-          for (var i = 0; i < e.length; i++) {
-            eArr.push(e[i].title, e[i].startDate, e[i].endDate, e[i].createdAt)
-          }
-          this.events = eArr
-        })
-        .catch(error => {
-          this.events = error
-        })
+    //   Api.get('/events')
+    //     .then(response => {
+    //       // this.events = response.data.events[0].title
+    //       var e = response.data.events
+    //       var eArr = []
+    //       for (var i = 0; i < e.length; i++) {
+    //         eArr.push(e[i].title, e[i].startDate, e[i].endDate, e[i].createdAt)
+    //       }
+    //       this.events = eArr
+    //     })
+    //     .catch(error => {
+    //       this.events = error
+    //     })
     }
   }
 }
