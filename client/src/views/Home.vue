3c4d989eadb195416c9e5f0f494283ff533259c0
Hugo Hempel <Hugohhempel@gmail.com>
2020-09-25 14:57:29 +0200

    added bar-list component to home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <header-bar></header-bar>
     <search-bar></search-bar>
+    <bar-list></bar-list>
     <bar-map></bar-map>
   </div>
 </template>
@@ -10,11 +11,13 @@
 import Header from '@/components/Header'
 import SearchBar from '@/components/SearchBar'
 import Map from '@/components/Map'
+import BarList from '@/components/BarList'
 export default {
   name: 'home',
   components: {
     'header-bar': Header,
     'search-bar': SearchBar,
+    'bar-list': BarList,
     'bar-map': Map
   }
 }
