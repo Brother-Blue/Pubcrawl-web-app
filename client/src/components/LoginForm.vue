d456245c01fc484a2286370ba97fc9bbc6e8e537
Christian O'Neill <gusonech@student.gu.se>
2020-09-30 10:33:18 +0200

    Updated login form

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -1,28 +1,85 @@
 <template>
-    <div class="form-container">
-            <b-form-group
+    <div>
+      <b-card
+      class="border border-warning bg-dark text-warning text-left login-container"
+      title="Login"
+      >
+      <hr class="bg-secondary"><br>
+        <b-form-group
+            class="text-light"
             id="fieldset-1"
-            description="Let us know your name."
+            label="Enter username"
+            label-for="input-1"
+            :invalid-feedback="invalidFeedback"
+            :valid-feedback="validFeedback"
+            >
+            <b-form-input
+            id="input-1"
+            v-model="username"
+            type="text"
+            :state="usernameState"
+            trim
+            ></b-form-input>
+
+            <b-form-invalid-feedback id="input-live-feedback">
+              Usernames must be between 4-15 characters
+            </b-form-invalid-feedback>
+        </b-form-group>
+            <b-form-group
+            class="text-light"
+            id="fieldset-1"
             label="Enter your name"
             label-for="input-1"
             :invalid-feedback="invalidFeedback"
             :valid-feedback="validFeedback"
-            :state="state"
             >
-            <b-form-input id="input-1" v-model="name" :state="state" trim></b-form-input>
-            </b-form-group>
-        </div>
+        <b-form-input
+        id="input-1"
+        v-model="password"
+        type="password"
+        :state="passwordState"
+        trim
+        ></b-form-input>
+        <b-form-invalid-feedback id="input-live-feedback">
+          Passwords must be between 6-128 characters
+        </b-form-invalid-feedback>
+
+        </b-form-group>
+        <p class="text-light">New user? <a href="#" class="text-warning">Register here.</a></p>
+        <b-button class="btn" variant="outline-warning">Login</b-button>
+      </b-card>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'login-form'
+  name: 'login-form',
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  computed: {
+    usernameState() {
+      return this.username.length >= 4 && this.username.length <= 15
+    },
+    passwordState() {
+      return this.password.length >= 6 && this.password.length <= 128
+    }
+  }
 }
 </script>
 
 <style scoped>
-.form-container {
-    max-width: 50%;
-    margin: auto;
+.login-container {
+  max-width: 50%;
+  margin: 75px auto;
+  box-shadow: 5px 5px 5px 3px black;
+}
+
+.btn {
+  width: 25%;
+  float: right;
 }
 </style>
