9bd8b4662cc2642f4e9f7e47f429c65902f12d54
hjalmar <hjalmar.thunberg@gmail.com>
2020-10-06 01:06:09 +0200

    Remade passport function, now works with JWT

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,30 +1,51 @@
 var User = require('../models/user');
 var passport = require('passport');
-var LocalStrategy = require('passport-local');
+var localStrategy = require('passport-local').Strategy;
+var JWTstrategy = require('passport-jwt').Strategy;
+var ExtractJWT = require('passport-jwt').ExtractJwt;
 
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.findOne({username: username}, function(err, user) {
-            if (err) return done(err);
-            if (!user) {
-              return done(null, false, {'message': 'incorrect username or password'});
-            }
-            if (!user.validPassword(password)) {
-              return done(null, false, {'message': 'incorrect username or password'});
-            }
-            return done(null, user);
-        });
+// TODO: add 'passport.authenticate('jwt', { session: false }),' to APIs that we want to protect
+
+passport.use('login', new localStrategy({
+      usernameField: 'username',
+      passwordField: 'password'
+    },
+    async (username, password, done) => {
+      try {
+        var user = await User.findOne({ username });
+
+        if (!user) {
+          return done(null, false, {'message': 'wrong username or password'});
+        }
+
+        var validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, {'message': 'wrong username or password'});
+        }
+
+        return done(null, user, {'message': 'Logged in Successfully'});
+      } catch (error) {
+        return done(error);
+      }
     }
-));
+  )
+);
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-  
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: 'TOP_SECRET',
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
 
 module.exports = passport
