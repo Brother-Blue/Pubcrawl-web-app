5480630c8269d57429ba2a4c57f1d7b50863a574
hjalmar <hjalmar.thunberg@gmail.com>
2020-10-12 14:13:32 +0200

    Styled bar rating

--- client/src/components/BarItem.vue
+++ client/src/components/BarItem.vue
@@ -13,13 +13,11 @@
     <b-list-group-item class="bar-attr bg-dark">
     <b-form-rating
     id="avgRating"
-    variant="warning"
-    class="bg-dark text-warning border-0"
+    variant="danger"
+    class="bg-dark border-0"
     v-if="barRating > 0"
     readonly
-    show-value
-    show-value-max
-    precision="1"
+    precision="2"
     :value="barRating"
     ></b-form-rating>
     <p class="text-secondary" v-if="barRating <= 0">No reviews</p>
