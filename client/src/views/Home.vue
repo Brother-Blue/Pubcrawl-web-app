0ff48c4e17b87e5898c1f2ff1e37226ffe61a2ad
hjalmar <hjalmar.thunberg@gmail.com>
2020-10-14 13:33:46 +0200

    Tried renaming cookie again

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -94,10 +94,11 @@ export default {
         .then(response => {
           if (response.data) {
             console.log('Has valid cookie')
+            console.log(response.data)
             localStorage.setItem('pubcrawl_user_id', response.data)
           } else {
             console.log('Has invalid cookie')
-            document.cookie = 'name=jwt; content=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+            document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
             localStorage.removeItem('pubcrawl_user_id')
           }
         }).catch(error => console.log(error))
