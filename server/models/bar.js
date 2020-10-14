4033d95083ac8867564f0ac14e4f5369da4de8c7
hjalmar <hjalmar.thunberg@gmail.com>
2020-10-14 10:12:06 +0200

    Remade presave to work 100%

--- server/models/bar.js
+++ server/models/bar.js
@@ -1,38 +1,42 @@
-var mongoose = require('mongoose');
-var Review = require('./review.js')
+var mongoose = require("mongoose");
+var Review = require("./review.js");
 
 // Create bar schema
-let BarSchema = new mongoose.Schema({
-    name: { type: String, required: true, minlength: [1, 'Minimum length is 1'], maxlength: [25, 'Maximum length is 25.'] },
+let BarSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: [1, "Minimum length is 1"],
+      maxlength: [25, "Maximum length is 25."],
+    },
     latLong: { type: [Number], required: true },
-    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'reviews' }],
-    events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'events' }],
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "reviews" }],
+    events: [{ type: mongoose.Schema.Types.ObjectId, ref: "events" }],
     address: { type: String },
     photo: { type: String },
-    averageRating: { type: Number, default: 0 }
-}, {
-    versionKey: false // Skip mongoose-version-key
-});
+    averageRating: { type: Number, default: 0 },
+  },
+  {
+    versionKey: false, // Skip mongoose-version-key
+  }
+);
 
-BarSchema.pre('save', async function(next) {
-    var bar = this;
+BarSchema.pre("save", async function () {
+  var bar = this;
+  
+  if (bar.reviews.length) {
+    var reviews = await Review.find({ _id: { $in: bar.reviews } }).exec();
 
-    if (bar.reviews.length > 0) {
-        bar.averageRating = 0;
-        var avg = 0;
-        var divide = 0;
+    var result = reviews.reduce((sum, current) => {
+      sum += current.averageRating;
+      return sum;
+    }, 0);
 
-        for (let i = 0; i < bar.reviews.length; i++) {
-            await Review.findById(bar.reviews[i], async function(err, review) {
-                avg += review.averageRating;
-                divide++;
-            })
-        }
-        avg /= divide;
-        bar.averageRating =  Number(avg.toFixed(1));
-    }
-    next();
+    var average = result / reviews.length;
+    bar.averageRating = Number(average.toFixed(1));
+  }
 });
 
 // Compile model from BarSchema
-module.exports = mongoose.model('bars', BarSchema);
+module.exports = mongoose.model("bars", BarSchema);
