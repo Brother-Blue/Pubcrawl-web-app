89fd7cd274dc6616d0b47b13ed1c376c0ea4ea6c
hjalmar <hjalmar.thunberg@gmail.com>
2020-10-15 18:18:53 +0200

    increasing the timeout

--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -241,8 +241,8 @@ export default {
         }).catch(error => {
           console.error(error)
         })
-      setTimeout(this.curKey += 1, 100)
-      setTimeout(this.getEvents(), 100)
+      setTimeout(this.curKey += 1, 1000)
+      setTimeout(this.getEvents(), 1000)
     },
     async deleteReviewForever(id) {
       await Api.delete(`/reviews/${id}`)
@@ -251,8 +251,8 @@ export default {
         }).catch(error => {
           console.error(error)
         })
-      setTimeout(this.curKey += 1, 100)
-      setTimeout(this.getReviews(), 100)
+      setTimeout(this.curKey += 1, 1000)
+      setTimeout(this.getReviews(), 1000)
     },
     updateEvent(id, payload) {
       Api.patch(`/events/${id}`, payload)
