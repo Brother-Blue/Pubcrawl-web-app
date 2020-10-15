75cc29305216e643b63888fda3a627b4e4cd345e
hjalmar <hjalmar.thunberg@gmail.com>
2020-10-15 16:18:46 +0200

    Creating review now pushes id to user

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,5 +1,6 @@
 var Event = require('../models/event');
 var Bar = require('../models/bar');
+var User = require('../models/user');
 var passport = require('passport');
 var express = require('express');
 
@@ -21,6 +22,14 @@ router.post('', passport.authenticate('jwt', { session: false }), function(req, 
     event.save(function(err) {
         if (err) { return next(err); }
         res.status(201).json(event);
+    })
+    User.findById({_id: req.body.users}, function(err, user) {
+        if (err) { return next(err); }
+        if (!user) { return res.status(404).json({'message': 'user not found'}) }
+        user.reviews.push(review._id)
+        user.save(function(err) {
+            if (err) { return next(err); }
+        })
     })
 });
 
